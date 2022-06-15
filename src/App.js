70734dd19b5656d9313477d16f09daac007a65fa
David Kuo <97716012+drkuo15@users.noreply.github.com>
2022-06-15 14:16:32 +0800

    feat: get data from firestore successfully

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from './firestore';
 
 function App() {
+  async function getData() {
+    const docRef = doc(db, 'recipes', 'O3pzlJ8g9gTtHahU9aeZ');
+    const docSnap = await getDoc(docRef);
+    console.log('Document data:', docSnap.data());
+  }
+
+  useEffect(() => { getData(); }, []);
+
   return (
     <div className="App">
       hello!
